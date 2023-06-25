be7dae5d63f35f9a8516620b252dd75de7fa80e5
Saad Aslam <saslam01@i2cinc.com>
2023-06-25 18:47:16 +0500

    Frontend:PreferredItemsList Component Added

--- frontend/src/core/Elements/PreferredArticles.jsx
+++ frontend/src/core/Elements/PreferredArticles.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PreferredItemsList } from "./PreferredItemsList";
 
 const PreferredArticles = ({
   loggedUser,
@@ -10,39 +11,18 @@ const PreferredArticles = ({
   return (
     <div>
       {loggedUser && (
-        <div className="mt-2">
-          <label className="block text-sm font-medium text-gray-700">
-            Preferred Authors:
-          </label>
-          <div className="flex gap-2">
-            {preferredAuthors.slice(0, maxDisplayedAuthors).map((author) => (
-              <div key={author.label} className="px-3 py-1 bg-gray-200 rounded">
-                {author.label}
-              </div>
-            ))}
-            {preferredAuthors.length > maxDisplayedAuthors && (
-              <div className="px-3 py-1 ">.. and more</div>
-            )}
-          </div>
-        </div>
-      )}
-
-      {loggedUser && (
-        <div className="mt-2">
-          <label className="block text-sm font-medium text-gray-700">
-            Preferred Sources:
-          </label>
-          <div className="flex gap-2">
-            {preferredSources.slice(0, maxDisplayedSources).map((source) => (
-              <div key={source.label} className="px-3 py-1 bg-gray-200 rounded">
-                {source.label}
-              </div>
-            ))}
-            {preferredSources.length > maxDisplayedSources && (
-              <div className="px-3 py-1  ">.. and more</div>
-            )}
-          </div>
-        </div>
+        <>
+          <PreferredItemsList
+            title="Preferred Authors"
+            items={preferredAuthors}
+            maxDisplayedItems={maxDisplayedAuthors}
+          />
+          <PreferredItemsList
+            title="Preferred Sources"
+            items={preferredSources}
+            maxDisplayedItems={maxDisplayedSources}
+          />
+        </>
       )}
     </div>
   );
